3456a812558affea4459069978934c5a2e056017
jorgesinga <jsinga@entel.bo>
2025-09-18 14:56:39 -0400

    Tabla Tarifa agregado

--- src/main/java/com/gestor/gestor/repository/entity/Tarifa.java
+++ src/main/java/com/gestor/gestor/repository/entity/Tarifa.java
@@ -1,4 +1,27 @@
 package com.gestor.gestor.repository.entity;
+import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "COMGES_SERVICIO_TARIFAS")
 public class Tarifa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_SERVICIO_TARIFA")
+    private Long id_tarifa;
+    @Column(name = "DESCRIPCION")
+    private String descripcion;
+    @Column(name = "COMBO_PLAY")
+    private String combo_play;
+    @Column(name = "PLAN_PAQUETE")
+    private String plan_paquete;
+    @Column(name = "COMPONENTE")
+    private String componente;
+    @Column(name = "TARIFA")
+    private long tarifa ;
+    @Column(name = "FECHA_ALTA")
+    private LocalDateTime fecha_alta;
+    @Column(name = "NEGOCIO")
+    private String negocio;
 }
