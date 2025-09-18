a89c16fd029b5f967e2285afad06153df323b399
Marisabel Condori Cano <mccondori@entel.bo>
2025-09-18 10:43:15 -0400

    commit incial - conexion base de datos

---
+++ src/main/java/com/gestor/gestor/controller/SaludoController.java
@@ -0,0 +1,15 @@
+package com.gestor.gestor.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/saludo")
+public class SaludoController {
+    
+    @GetMapping("/hola")
+    public String saludo(){
+        return "Hola que tal";
+    }
+}
