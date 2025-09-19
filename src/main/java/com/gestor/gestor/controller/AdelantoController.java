356490db9e4b042c3d465dff871230cd5247130c
Marisabel Condori Cano <mccondori@entel.bo>
2025-09-19 16:39:21 -0400

    cambios adicionales

--- src/main/java/com/gestor/gestor/controller/AdelantoController.java
+++ src/main/java/com/gestor/gestor/controller/AdelantoController.java
@@ -2,9 +2,6 @@ package com.gestor.gestor.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +17,4 @@ public class AdelantoController {
     public List<AdelantoDTO> obtenerListaAdelanto(){
         return this.adelantoService.obtenerListaAdelanto();
     }
-
-    
 }
